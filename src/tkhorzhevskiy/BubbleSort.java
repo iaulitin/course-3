e8fe503be8db0b3ac439575142e5ec42d2566723
dmithorzh <dmithor@yandex.ru>
2021-10-23 20:27:00 +0300

    Исправил ошибки.

--- src/tkhorzhevskiy/BubbleSort.java
+++ src/tkhorzhevskiy/BubbleSort.java
@@ -20,8 +20,7 @@ public class BubbleSort {
             for (int i = 0; i < arr.length - 1; i++) {
                 if (arr[i] > arr[i + 1]) {
                     isSorted = false;
-                    int temp;
-                    temp = arr[i];
+                    int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
                 }
